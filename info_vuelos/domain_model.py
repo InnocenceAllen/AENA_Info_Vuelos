2da483d993d67d1d4f55b1dbfd31cf1cd31307ab
Mario Gómez Martínez <magomar@gmail.com>
2018-03-30 00:09:32 +0200

    Retrieves both departures and arrivals

--- info_vuelos/domain_model.py
+++ info_vuelos/domain_model.py
@@ -1,16 +1,24 @@
+class Airport:
+    def __init__(self, code, name):
+        self.code = code
+        self.name = name
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.code)
+
+
 class Flight:
-    def __init__(self, date, time, flight_number, origin, origin_code, destiny, destiny_code, company, terminal):
+    def __init__(self, date, time, flight_number, origin, destiny, company, terminal, url):
         self.flightNumber = flight_number
         self.origin = origin
         self.date = date
         self.time = time
         self.origin = origin
-        self.originCode = origin_code
         self.destiny = destiny
-        self.destinyCode = destiny_code
         self.company = company
         self.terminal = terminal
+        self.url = url
 
     def __str__(self):
         return '{} {} {} from {} to {}'.format(self.date, self.time, self.flightNumber, self.origin,
-                                                         self.destiny)
+                                               self.destiny)
--- info_vuelos/scraper.py
+++ info_vuelos/scraper.py
@@ -1,8 +1,7 @@
 import requests
 from info_vuelos import constants
-from info_vuelos.Flight import Flight
+from info_vuelos.domain_model import Flight, Airport
 from bs4 import BeautifulSoup
-
 
 def getContent(url):
     content = requests.get(url).content
@@ -12,26 +11,17 @@ def getLocalContent(filename):
     fileHandler = open(filename, encoding="utf8")
     return BeautifulSoup(fileHandler, "html.parser")
 
+def getDeparturesContent(airportCode):
+    return getContent(constants.AENA_INFOVUELOS_URL + constants.DEPARTURES + airportCode)
+
+def getArrivalsContent(airportCode):
+    return getContent(constants.AENA_INFOVUELOS_URL + constants.ARRIVALS + airportCode)
+
 def getAirportName(fullname):
     return fullname[: fullname.find("(")].strip()
 
 def getAirportCode(fullname):
     return fullname[fullname.find("(") + 1:fullname.find(")")].strip()
-
-def get_airports(soup):
-    airports = []
-    origin = soup.find(id="origin_ac")
-    resultSet = origin.find_all("option")
-    for op in resultSet:
-        airports.append([op['value'], getAirportName(op.text)])
-    airports.pop(0)
-    return airports
-
-def getDepartures(airportCode):
-    return getContent(constants.AENA_INFOVUELOS_URL + constants.DEPARTURES + airportCode)
-
-def getArrivals(airportCode):
-    return getContent(constants.AENA_INFOVUELOS_URL + constants.ARRIVALS + airportCode)
 
 def getDetails(flightURL):
     return getContent(constants.AENA_BASE_URL + flightURL)
@@ -39,40 +29,61 @@ def getDetails(flightURL):
 def clean(rawText):
     return rawText.replace("\n", "").strip()
 
+def get_airports(soup):
+    airports = []
+    origin = soup.find(id="origin_ac")
+    resultSet = origin.find_all("option")
+    for op in resultSet:
+        aiport = Airport(op['value'], getAirportName(op.text))
+        airports.append(aiport)
+    airports.pop(0)
+    return airports
+
+def getFlightInfo(row):
+    cells = row.find_all("td")
+    time = cells[0].text
+    flightNumber = cells[1].a.text.strip()
+    url = cells[1].a['href']
+    airport = Airport(getAirportCode(cells[2].text), getAirportName(cells[2].text))
+    company = cells[3].text.strip()
+    terminal = cells[4].text
+    return time, flightNumber, airport, company, terminal, url
+
+def getDepartures(airport):
+    flights = []
+    soup = getDeparturesContent(airport.code)
+    tables = soup.find(id="flightResults").findAll("table")
+    for table in tables:
+        date = clean(table.caption.text.split(",")[-1])
+        rows = table.find("tbody").find_all("tr")
+        for row in rows:
+            if (row["class"][0] == "principal"):
+                time, flightNumber, destiny, company, terminal, url = getFlightInfo(row)
+                flight = Flight(date, time, flightNumber, airport, destiny, company, terminal, url)
+                flights.append(flight)
+    return flights
+
+def getArrivals(airport):
+    flights = []
+    soup = getArrivalsContent(airport.code)
+    tables = soup.find(id="flightResults").findAll("table")
+    for table in tables:
+        date = clean(table.caption.text.split(",")[-1])
+        rows = table.find("tbody").find_all("tr")
+        for row in rows:
+            if (row["class"][0] == "principal"):
+                time, flightNumber, origin, company, terminal, url = getFlightInfo(row)
+                flight = Flight(date, time, flightNumber, origin, airport, company, terminal, url)
+                flights.append(flight)
+    return flights
+
 
 #soup = getLocalContent(constants.LOCAL_INFOVUELOS_URL)
 soup = getContent(constants.AENA_INFOVUELOS_URL)
 airports = get_airports(soup)
+print('Airports\n' + ''.join(str(a)+'; ' for a in airports))
 
-airportCode = airports[0][0]
-airportName = airports[0][1]
-
-departures = []
-arrivals = []
-
-#soup = getLocalContent(constants.LOCAL_SAMPLE_DEPARTURES)
-soup = getDepartures(airportCode)
-
-flightResultsTag = soup.find(id="flightResults")
-tables = flightResultsTag.findAll("table")
-tableIndex = 0
-for table in tables:
-    date = clean(table.caption.text.split(",")[-1])
-    rows = table.find("tbody").find_all("tr")
-    for row in rows:
-        if (row["class"][0] == "principal"):
-            cells = row.find_all("td")
-            time = cells[0].text
-            flightNumber = cells[1].a.text.strip()
-            url = cells[1].a['href']
-            origin = airportName
-            originCode = airportCode
-            destiny = getAirportName(cells[2].text)
-            destinyCode = getAirportCode(cells[2].text)
-            company = cells[3].text.strip()
-            terminal = cells[4].text
-            flight = Flight(date, time, flightNumber, origin, originCode, destiny, destinyCode, company, terminal)
-            departures.append(flight)
-            print(flight)
-
-
+departures = getDepartures(airports[0])
+arrivals = getArrivals(airports[0])
+print('Departures\n' + ''.join(str(f) + '\n ' for f in departures))
+print('Arrivals\n' + ''.join(str(f) + '\n ' for f in arrivals))
