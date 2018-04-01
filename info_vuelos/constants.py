78a1cc0c627776132f98dc38dec2c9bb5ab588d4
caos <helmut.enockl@gmail.com>
2018-04-01 16:29:08 +0200

    [features/initCSV] Ahora ya explora y genera el CSV(1.9 k de filas) con la información extra de vuelo. Incluso podría contener información en las escalas de vuelo. (tarda 3 min en generar toda la info)

--- info_vuelos/constants.py
+++ info_vuelos/constants.py
@@ -1,4 +1,5 @@
 AENA_INFOVUELOS_URL = "http://www.aena.es/csee/Satellite/infovuelos/es/"
+AENA_URL = "http://www.aena.es/"
 LOCAL_INFOVUELOS_URL = "resources/infovuelos.html"
 LOCAL_SAMPLE_DEPARTURES = "resources/lcg-departures.html"
 DEPARTURES = "?destiny=&mov=S&origin_ac="
--- info_vuelos/scraper.py
+++ info_vuelos/scraper.py
@@ -5,19 +5,67 @@ import utils
 import re
 from bs4 import BeautifulSoup
 
+''''
+Nota:Helmut
+Este espacio es para especificar las mejoras.
+Falta crear una clase y mover el objeto FlightLine
+Falta añadir las llegadas ( es llamar a la parte de salidas que tenemos pero con la URL de llegadas)
+Falta añadir la verificación de vuelos duplicados como indico Mario.
+
+'''
 
 class FlightLine:
-    def __init__(self,airportCode, hora, destino, vuelo,cia,terminal):
+    def __init__(self,airportCode, hora, destino, vuelo,cia,terminal,url):
         self.airportCode = airportCode.replace("\n", "").strip()
         self.hora = hora.replace("\n","").strip()
         self.destino = destino.replace("\n","").strip()
         self.vuelo = vuelo.replace("\n","").strip()
         self.cia = cia.replace("\n", "").strip()
         self.terminal = terminal.replace("\n", "").strip()
+        self.url=url;
+        self.flightsInfo =loadFromURL(url);
+        #print(self);
 
     def __str__(self):
+        str=airportCode+";"+self.hora+";"+self.destino+";"+self.vuelo+";"+self.cia+";"+self.terminal
+        for flightInfo in  self.flightsInfo:
+            str=str+flightInfo
+        return str
 
-        return airportCode+";"+self.hora+";"+self.destino+";"+self.vuelo+";"+self.cia+";"+self.terminal
+
+
+
+def loadFromURL(url):
+        #print (url)
+        soup=BeautifulSoup(crawler.getContent(url), "html.parser")
+
+        try:
+            tables = soup.findAll("table")
+        except:
+            pass;
+        tableIndex = 0
+        flightInfo = []
+        for table in tables:
+            rows = table.find("tbody").find_all("tr")
+
+            for row in rows:
+                #print(getFlightInfo(row))
+                flightInfo.append(getFlightInfo(row))
+
+
+        return  flightInfo
+
+def getFlightInfo(row):
+    cells = row.find_all("td")
+    date = cells[0].text
+    time = cells[1].text
+    terminal = cells[2].text
+    counter = cells[3].text.strip()
+    door = cells[4].text
+    state=cells[5].text
+
+    #return date,time, counter, door, terminal, state
+    return ";"+ date+";"+ time+";"+ counter+";"+door+";"+ terminal+";"+ state
 
 
 def getContent(url):
@@ -54,8 +102,8 @@ code = airports[0][0]
 flightLines=[]
 for airport in airports:
     airportCode=airport[0]
-    print(airportCode)
-    soup = BeautifulSoup(getDepartures(airportCode))
+    #print(airportCode)
+    soup = BeautifulSoup(getDepartures(airportCode),"html.parser")
 
     flightResultsTag = soup.find(id="flightResults")
     try:
@@ -69,6 +117,7 @@ for airport in airports:
         print(when)'''
         rows = table.find("tbody").find_all("tr")
         for row in rows:
+            flightURL=constants.AENA_URL+ row.a['href']
             flightNumer = row.a.text
 
             '''hora=cleanhtml(str(row.find(headers="hora_0")))
@@ -80,11 +129,11 @@ for airport in airports:
 
 
 
-            flightLines.append(FlightLine(airportCode,hora,destino,flightNumer,cia,terminal))
+            flightLines.append(FlightLine(airportCode,hora,destino,flightNumer,cia,terminal,flightURL))
             '''hora=soup.find_all(headers="hora_1")'''
-            print("--------------------------------")
-            print(hora +""+destino+""+ flightNumer+""+cia+""+terminal)
-            print("--------------------------------")
-    utils.createCSV("test2.csv", flightLines)
+            #print("--------------------------------")
+            #print(hora +""+destino+""+ flightNumer+""+cia+""+terminal)
+            #print("--------------------------------")
+    utils.createCSV("test4.csv", flightLines)
 
 
