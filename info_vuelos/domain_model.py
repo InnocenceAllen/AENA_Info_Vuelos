126cd3031803fb9963f34af78cf9f242684d93d4
Mario Gómez Martínez <magomar@gmail.com>
2018-04-04 21:00:04 +0200

    working on new flight model with separated info for departure and arriva

--- info_vuelos/domain_model.py
+++ info_vuelos/domain_model.py
@@ -1,3 +1,9 @@
+from enum import Enum
+class FlightType(Enum):
+    NATIONAL = 1
+    INTERNATIONAL_ARRIVAL = 2
+    INTERNATIONAL_DEPARTURE = 3
+
 class Airport:
     def __init__(self, code, name):
         self.code = code
@@ -15,23 +21,45 @@ class Airport:
         return hash(self.code)
 
 class Flight:
-    def __init__(self, date, time, flight_number, origin, destiny, company, terminal, url):
-        self.date = date
-        self.time = time
+    def __init__(self, flight_number, company, plane, departure, arrival):
         self.flightNumber = flight_number
-        self.origin = origin
-        self.destiny = destiny
         self.company = company
-        self.terminal = terminal
-        self.url = url
+        self.plane = plane
+        self.departure = departure
+        self.arrival = arrival
 
     def __str__(self):
-        return '{} {} {} from {} to {}'.format(self.date, self.time, self.flightNumber, self.origin,
-                                               self.destiny)
+        return '{}: [{}] -> [{}]'.format(self.flightNumber, str(self.departure),  str(self.arrival))
+
 
     def __eq__(self, other):
         if isinstance(self, other.__class__):
-            return (self.date, self.flightNumber) == (other.date, other.flightNumber)
+            return (self.flightNumber, self.departure, self.arrival) == (other.flightNumber, other.departure, other.arrival)
 
     def __hash__(self):
-        return hash((self.date, self.flightNumber))
+        return hash((self.flightNumber, self.departure, self.arrival))
+
+
+class FlightSchedule:
+    def __init__(self, date, time, airport, terminal, status, weather):
+        self.date = date
+        self.time = time
+        self.airport = airport
+        self.terminal = terminal
+        self.status = status
+        self.weather = weather
+
+    def __str__(self):
+        return '{} {} {} {} {} {}'.format(self.date, self.time, self.airport, self.terminal, self.status, self.weather)
+
+class Departure (FlightSchedule):
+    def __init__(self, date, time, airport, terminal, status, counter, door):
+        FlightSchedule.__init__(date, time, airport, terminal, status)
+        self.counter = counter
+        self.door = door
+
+class Arrival (FlightSchedule):
+    def __init__(self, date, time, terminal, status, room, belt):
+        FlightSchedule.__init__(date, time, terminal, status)
+        self.room = room
+        self.belt = belt
--- info_vuelos/scraper.py
+++ info_vuelos/scraper.py
@@ -1,4 +1,4 @@
-from info_vuelos.domain_model import Flight, Airport
+from info_vuelos.domain_model import Flight, Airport, FlightSchedule
 from info_vuelos import util
 
 def get_airports(soup):
@@ -13,13 +13,48 @@ def get_airports(soup):
 
 def getFlightInfo(row):
     cells = row.find_all("td")
-    time = cells[0].text
+    #time = cells[0].text
     flightNumber = cells[1].a.text.strip()
     url = cells[1].a['href']
-    airport = Airport(util.getAirportCode(cells[2].text), util.getAirportName(cells[2].text))
+    #airport = Airport(util.getAirportCode(cells[2].text), util.getAirportName(cells[2].text))
     company = cells[3].text.strip()
-    terminal = cells[4].text
-    return time, flightNumber, airport, company, terminal, url
+    #terminal = cells[4].text
+
+    plane, departure, arrival = getFlightDetails(url)
+    return Flight(flightNumber, company, plane, departure, arrival)
+
+def getFlightDetails(relativeUrl):
+    soup = util.getDetails(relativeUrl)
+    table = soup.find("table")
+    plane = table.caption.find_all("span")[1]
+
+    theads = table.find_all("thead")
+    tbodys = table.find_all("tbody")
+
+    departure = getFlightSchedule(theads[0], tbodys[0])
+    if (len(tbodys)>1):
+        arrival =  getFlightSchedule(theads[1], tbodys[1])
+    else:
+        arrival = getFlightSchedule(theads[1], None)
+
+    return plane, departure, arrival
+
+def getFlightSchedule(thead, tbody):
+    tr = thead.tr
+    try:
+        airport = tr.th.a.text
+        weather = tr.img["alt"]
+        cells = tbody.tr.find_all("td", )
+        date = cells[0].text
+        time = cells[1].text
+        terminal = cells[2].text
+        status = cells[5].text
+        return FlightSchedule(date, time, airport, terminal, status, weather)
+    except AttributeError:
+        airport = tr.find_all("span")[1].text
+        return Airport(util.getAirportCode(airport), util.getAirportName(airport))
+
+
 
 def getDepartures(airport):
     flights = []
@@ -30,8 +65,8 @@ def getDepartures(airport):
         rows = table.find("tbody").find_all("tr")
         for row in rows:
             if row["class"][0] == "principal":
-                time, flightNumber, destiny, company, terminal, url = getFlightInfo(row)
-                flight = Flight(date, time, flightNumber, airport, destiny, company, terminal, url)
+                flight = getFlightInfo(row)
+                print(flight)
                 flights.append(flight)
     return flights
 
@@ -44,8 +79,8 @@ def getArrivals(airport):
         rows = table.find("tbody").find_all("tr")
         for row in rows:
             if row["class"][0] == "principal":
-                time, flightNumber, origin, company, terminal, url = getFlightInfo(row)
-                flight = Flight(date, time, flightNumber, origin, airport, company, terminal, url)
+                flight = getFlightInfo(row)
+                print(flight)
                 flights.append(flight)
     return flights
 
@@ -53,16 +88,16 @@ airports = get_airports(util.getAirportsContent())
 print('Airports\n' + ''.join(str(a)+'; ' for a in airports))
 
 flights = []
-uniqueFlights = set()
+#uniqueFlights = set()
 for airport in airports:
     departures = getDepartures(airport)
     arrivals = getArrivals(airport)
     flights = flights + departures + arrivals
-    uniqueFlights.update(departures)
-    uniqueFlights.update(arrivals)
+    #uniqueFlights.update(departures)
+    #uniqueFlights.update(arrivals)
 
 
-print('Number of flights including duplicates = {}'.format(len(flights)))
-print('Number of flights removing duplicates = {}'.format(len(uniqueFlights)))
+print('Number of flights (departures + arrivals) = {}'.format(len(flights)))
+#print('Number of flights removing duplicates = {}'.format(len(uniqueFlights)))
 
 #print('Flights\n' + ''.join(str(f) + '\n ' for f in flights))
