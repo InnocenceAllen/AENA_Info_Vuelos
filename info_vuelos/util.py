f74570a8136fcbdc6630e45cb245956f85f99ff9
magomar <magomar@gmail.com>
2018-04-08 18:41:02 +0200

    fixes error generating the csv (it was writing one character per line instead of one flight per line)

--- info_vuelos/util.py
+++ info_vuelos/util.py
@@ -59,4 +59,4 @@ def unique_everseen(iterable):
 def save_to_csv(filename, data):
     with open(filename, 'a', newline='', encoding='utf8') as f:
         writer = csv.writer(f)
-        writer.writerows(map(lambda x: [x], repr(data)))
+        writer.writerows(map(lambda x: [repr(x)], data))
