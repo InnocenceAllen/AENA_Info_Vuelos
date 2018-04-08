5bead58f03c8259ba0bc60c7a5e1cf1c1c4c0e51
magomar <magomar@gmail.com>
2018-04-08 17:55:44 +0200

    generates csv using repr methods

--- info_vuelos/scraper.py
+++ info_vuelos/scraper.py
@@ -2,16 +2,16 @@ import time
 import logging as log
 import sys
 import constants
-from info_vuelos.domain_model import Flight, Airport, FlightInfoMode, FlightType, Departure, Arrival
 from info_vuelos.domain_model import Flight, Airport, FlightInfoMode, FlightType, Departure, Arrival, Weather
 from info_vuelos import util
 
-#global output
-orig_stdout = sys.stdout
-date=time.strftime("%d-%m-%Y_%I-%M")
-f = open('flight'+date+'.csv', 'w')
-sys.stdout = f
-print(constants.HEADER)
+
+# global output
+# orig_stdout = sys.stdout
+# date = time.strftime("%d-%m-%Y_%I-%M")
+# f = open('flight' + date + '.csv', 'w')
+# sys.stdout = f
+# print(constants.HEADER)
 
 def get_airports(soup):
     airports = []
@@ -23,19 +23,21 @@ def get_airports(soup):
         airports.append(aiport)
     return airports
 
+
 def getFlightInfo(row, flightInfoMode):
     cells = row.find_all("td")
-    #time = cells[0].text
+    # time = cells[0].text
     flightNumber = cells[1].a.text.strip()
     url = cells[1].a['href']
-    #airport = Airport(util.getAirportCode(cells[2].text), util.getAirportName(cells[2].text))
+    # airport = Airport(util.getAirportCode(cells[2].text), util.getAirportName(cells[2].text))
     company = cells[3].text.strip()
-    #terminal = cells[4].text
+    # terminal = cells[4].text
     log.info("Scrapping  flight " + flightNumber)
     plane, departure, arrival, type = getFlightDetails(url, flightInfoMode)
     flight = Flight(flightNumber, company, plane, departure, arrival, type, url, time.ctime())
     print(flight)
     return flight
+
 
 def getFlightDetails(relativeUrl, flightInfoMode):
     soup = util.getDetails(relativeUrl)
@@ -46,7 +48,7 @@ def getFlightDetails(relativeUrl, flightInfoMode):
         theads = table.find_all("thead")
         tbodys = table.find_all("tbody")
 
-        if (len(tbodys)>1):
+        if (len(tbodys) > 1):
             departure = getFlightDepartureDetails(theads[0], tbodys[0])
             arrival = getFlightArrivalDetails(theads[1], tbodys[1])
             flight_type = FlightType.NATIONAL
@@ -57,28 +59,26 @@ def getFlightDetails(relativeUrl, flightInfoMode):
                 flight_type = FlightType.INTERNATIONAL_DESTINY
             else:
                 departure = getFlightDepartureDetails(theads[0], None)
-                arrival =  getFlightArrivalDetails(theads[1], tbodys[0])
+                arrival = getFlightArrivalDetails(theads[1], tbodys[0])
                 flight_type = FlightType.INTERNATIONAL_ORIGIN
         return plane, departure, arrival, flight_type
     else:
-        try:
-            log.ERROR("Error scraping flight details from %s"+ str(relativeUrl))
-        except:
-            pass
+        log.error("Error scraping flight details from %s",relativeUrl)
         return None, None, None, None
+
 
 def getFlightDepartureDetails(thead, tbody):
     tr = thead.tr
     try:
         airport_txt = tr.th.a.text
         airport = Airport(util.getAirportCode(airport_txt), util.getAirportName(airport_txt))
-        weather_section = tr.find("span", {"class":"clima"})
+        weather_section = tr.find("span", {"class": "clima"})
         weather_temp = weather_section.contents[1].split()
         weather_desc = tr.img["alt"]
         min_temp = int(weather_temp[0][:-1])
         max_temp = int(weather_temp[1][:-1])
         weather = Weather(min_temp, max_temp, weather_desc)
-        #weather = "{} {}".format(weather_temp, weather_desc)
+        # weather = "{} {}".format(weather_temp, weather_desc)
         cells = tbody.tr.find_all("td", )
         date = cells[0].text
         time = cells[1].text
@@ -96,12 +96,13 @@ def getFlightDepartureDetails(thead, tbody):
         log.warning('Some field is missing: %s', error)
         return Airport(util.getAirportCode(airport_txt), util.getAirportName(airport_txt))
 
+
 def getFlightArrivalDetails(thead, tbody):
     tr = thead.tr
     try:
         airport_txt = tr.th.a.text
         airport = Airport(util.getAirportCode(airport_txt), util.getAirportName(airport_txt))
-        weather_section = tr.find("span", {"class":"clima"})
+        weather_section = tr.find("span", {"class": "clima"})
         weather_temp = weather_section.contents[1]
         weather_desc = tr.img["alt"]
         weather = "{} {}".format(weather_temp, weather_desc)
@@ -122,22 +123,24 @@ def getFlightArrivalDetails(thead, tbody):
         log.warning('Some field is missing: %s', error)
         return Airport(util.getAirportCode(airport_txt), util.getAirportName(airport_txt))
 
+
 def getDepartures(airport):
     flights = []
     soup = util.getDeparturesContent(airport.code)
     try:
         tables = soup.find(id="flightResults").findAll("table")
         for table in tables:
-            #date = table.caption.text.split(",")[-1].strip()
+            # date = table.caption.text.split(",")[-1].strip()
             rows = table.find("tbody").find_all("tr")
             for row in rows:
                 if row["class"][0] == "principal":
                     flight = getFlightInfo(row, FlightInfoMode.DEPARTURE)
                     flights.append(flight)
-                    log.info('Departure: %s',flight)
+                    log.info('Departure: %s', flight)
     except AttributeError:
         log.error('Error scrapping departures from airport %s', airport)
     return flights
+
 
 def getArrivals(airport):
     flights = []
@@ -145,18 +148,20 @@ def getArrivals(airport):
     try:
         tables = soup.find(id="flightResults").findAll("table")
         for table in tables:
-            #date = table.caption.text.split(",")[-1].strip()
+            # date = table.caption.text.split(",")[-1].strip()
             rows = table.find("tbody").find_all("tr")
             for row in rows:
                 if row["class"][0] == "principal":
                     flight = getFlightInfo(row, FlightInfoMode.ARRIVAL)
                     flights.append(flight)
-                    log.info('Arrival: %s',flight)
+                    log.info('Arrival: %s', flight)
     except AttributeError:
         log.error('Error scraping arrivals to airport %s', airport)
     return flights
 
+
 def main():
+    filename = 'flights{}.csv'.format(time.strftime("%d-%m-%Y_%I-%M"))
 
     airports = get_airports(util.getAirportsContent())
     log.info('Scrapping airport names')
@@ -167,13 +172,16 @@ def main():
     flights = []
     for airport in airports:
         departures = getDepartures(airport)
+        util.save_to_csv(filename, departures)
         arrivals = getArrivals(airport)
-        flights = flights + departures + arrivals
+        util.save_to_csv(filename, departures)
+        # flights = flights + departures + arrivals
 
     log.info('Number of flights (departures + arrivals) = {}'.format(len(flights)))
 
 
 if __name__ == "__main__":
-    #log.basicConfig(filename='scrapping.log', level=log.INFO)
-    log.basicConfig(filename='scrapping.log', level=log.INFO, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S')
+    # log.basicConfig(filename='scrapping.log', level=log.INFO)
+    log.basicConfig(filename='scrapping.log', level=log.INFO, format='%(asctime)s %(message)s',
+                    datefmt='%d/%m/%Y %I:%M:%S')
     main()
