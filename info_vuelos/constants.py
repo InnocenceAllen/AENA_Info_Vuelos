46776adbaf4b93e861343acb83e680edc19375d4
magomar <magomar@gmail.com>
2018-04-09 10:02:11 +0200

    fixes csv headers (airport is divided into name and code, and weather into min temp, max temp and description)

--- info_vuelos/constants.py
+++ info_vuelos/constants.py
@@ -2,5 +2,5 @@ AENA_INFOVUELOS_URL = "http://www.aena.es/csee/Satellite/infovuelos/es/"
 AENA_BASE_URL = "http://www.aena.es"
 DEPARTURES = "?destiny=&mov=S&origin_ac="
 ARRIVALS ="?origin=&mov=L&origin_ac="
-DATA_FIELDS=['flightNumber', 'plane', 'dep_date', 'dep_time', 'dep_airport', 'dep_terminal', 'dep_status', 'dep_weather', 'dep_counter', 'dep_door', 'arr_date', 'arr_time', 'arr_airport', 'arr_terminal', 'arr_status', 'arr_weather', 'arr_room', 'arr_belt', 'timestamp']
+DATA_FIELDS=['flightNumber', 'plane', 'dep_date', 'dep_time', 'dep_airport_name', 'dep_airport_code', 'dep_terminal', 'dep_status', 'dep_weather_min', 'dep_weather_max', 'dep_weather_desc', 'dep_counter', 'dep_door', 'arr_date', 'arr_time', 'arr_airport_name', 'arr_airport_code', 'arr_terminal', 'arr_status', 'arr_weather_min', 'arr_weather_max', 'arr_weather_desc', 'arr_room', 'arr_belt', 'timestamp']
 CSV_DELIMITER = ';'
